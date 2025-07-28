52f74629a10809485f0a7e67a0b911fd4309a0fc
NAVEEN171 <naveenkumar171837@gmail.com>
2025-07-28 11:08:07 +0530

    angular material UI usage

--- assignment-14/angular-UI-Usage/src/app/app.ts
+++ assignment-14/angular-UI-Usage/src/app/app.ts
@@ -9,5 +9,5 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
   styleUrl: './app.css',
 })
 export class App {
-  protected title = 'angular-UI-Usage';
+  protected title = 'angular material UI';
 }
