f4449a737016bfccd9fcb725380401d781473d86
NAVEEN171 <naveenkumar171837@gmail.com>
2025-07-28 12:24:21 +0530

    Dependency Injection

--- assignment-15/Dependency-Injection/src/app/app.ts
+++ assignment-15/Dependency-Injection/src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DashboardComponent } from './components/dashboard-component/dashboard-component';
+
 import { WeatherModule } from './weather/weather-module';
 
 @Component({
