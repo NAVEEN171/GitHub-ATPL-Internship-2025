47ed2eb6d746ffa8e552cca937ffb135a5c3f537
NAVEEN171 <naveenkumar171837@gmail.com>
2025-08-01 12:38:48 +0530

    updated assignment-18

--- assignment-18/components-communication/src/app/app.ts
+++ assignment-18/components-communication/src/app/app.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected title = 'components-communication';
+  protected title = 'components communication';
 }
