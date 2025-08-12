520dbd8ede08c21bc6c63850a870f39c0c807f5a
NAVEEN171 <naveenkumar171837@gmail.com>
2025-08-12 08:39:02 +0530

    fixed typo

--- assignment-3/ConcatCls.java
+++ assignment-3/ConcatCls.java
@@ -5,25 +5,25 @@ public class ConcatCls {
         return  (endTime-startTime)/(Math.pow(10,-9));
     }
     public static void main(String[] args) {
-        long initialStartime=System.nanoTime();
+        long initialStartTime=System.nanoTime();
         String str1="Hello";
     
        
         str1=str1+" harry";
         System.out.println("Normal string appending...");
-        System.out.println(convertToSeconds(initialStartime,System.nanoTime())+"seconds");
+        System.out.println(convertToSeconds(initialStartTime,System.nanoTime())+"seconds");
 
-        initialStartime=System.nanoTime();
+        initialStartTime=System.nanoTime();
         System.out.println("String Buffer ...");
         StringBuffer str2=new StringBuffer("hello");
         str2.append("harry");
-        System.out.println(convertToSeconds(initialStartime,System.nanoTime())+"seconds");
+        System.out.println(convertToSeconds(initialStartTime,System.nanoTime())+"seconds");
 
-        initialStartime=System.nanoTime();
+        initialStartTime=System.nanoTime();
          System.out.println("String Builder...");
         StringBuilder str3=new StringBuilder("hello");
         str3.append("harry");
-        System.out.println(convertToSeconds(initialStartime,System.nanoTime())+"seconds");
+        System.out.println(convertToSeconds(initialStartTime,System.nanoTime())+"seconds");
         
     }
 }
