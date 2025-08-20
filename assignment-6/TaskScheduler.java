5c9d6417b15b9aeae9bea72b7a9b2d44c30acfd2
NAVEEN171 <naveenkumar171837@gmail.com>
2025-08-20 13:51:09 +0530

    updated assignment-6

--- assignment-6/TaskScheduler.java
+++ assignment-6/TaskScheduler.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -7,13 +8,42 @@ import java.util.Queue;
 public class TaskScheduler {
     Map<String, Queue<String>> tasks = new HashMap<>();
 
-    public void constructQueue() {
-        Queue<String> queue1 = new LinkedList<>(Arrays.asList("hello", "hello"));
-        System.out.println(queue1);
+    public void AddTask(ArrayList<String> currentTasks, String Day) {
+        Queue<String> queue1 = new LinkedList<>(currentTasks);
+        tasks.put(Day, queue1);
+    }
+
+    public void printTasks() {
+        System.out.println("------------------------");
+
+        System.out.println("printing task....");
+        for (Map.Entry<String, Queue<String>> entry : tasks.entrySet()) {
+            System.out.print(entry.getKey() + " - ");
+            System.out.println(entry.getValue());
+        }
+        System.out.println("------------------------");
+
+    }
+
+    public void Removetask(String task) {
+        if (tasks.containsKey(task)) {
+            tasks.remove(task);
+        } else {
+            System.out.println("Task Not Found ! ");
+        }
     }
 
     public static void main(String[] args) {
 
         TaskScheduler ts = new TaskScheduler();
+        ts.AddTask(new ArrayList<>(Arrays.asList("OfficeWork", "Bring Groceries", "Have Food")), "Monday");
+        ts.AddTask(new ArrayList<>(Arrays.asList("OfficeWork", "Bring Groceries", "Have Dinner")), "Tuesday");
+        ts.AddTask(new ArrayList<>(Arrays.asList("OfficeWork", "Bring Groceries", "Have Dinner")), "Wednesday");
+        ts.printTasks();
+        ts.Removetask("Thursday");
+        ts.printTasks();
+        ts.Removetask("Monday");
+        ts.printTasks();
+
     }
 }
