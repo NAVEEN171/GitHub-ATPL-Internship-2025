2920e6350f728cf34e3ee49d15f7627508709c95
NAVEEN171 <naveenkumar171837@gmail.com>
2025-08-25 08:22:06 +0530

    updated sql

---
+++ SQL/assignment1.sql
@@ -0,0 +1,151 @@
+show databases;
+
+use intern;
+
+show tables;
+
+
+
+
+create table Department_Naveen(
+    dept_id varchar(10) ,
+    dept_name varchar(50),
+    primary key(dept_id)
+);
+
+describe Department_Naveen;
+
+alter table Department_Naveen
+modify  dept_name varchar(50) NOT NULL;
+
+alter table Department
+modify  dept_id varchar(20) NOT NULL;
+insert into Department_Naveen values("D1","Computer Science"),("D2","Mathematics"),("D3","Physics");
+insert into Department_Naveen values("D4","Social");
+select * from Department_Naveen;
+
+
+create table courses_Naveen(
+course_id varchar(10) ,
+course_name varchar(20),
+dept_id varchar(10),
+primary key(course_id),
+foreign key (dept_id) references Department_Naveen(dept_id)
+);
+
+insert into courses_Naveen values("C1","Python","D1"),("C2","Java","D2"),("C3","CVRV","D2"),("C4","NMOD","D2"),("C5","Quantum Mathematics","D3");
+select * from courses_Naveen;
+
+select *
+from Department_Naveen as d
+right join courses_Naveen as c on d.dept_id=c.dept_id;
+
+create table students_Naveen(
+    student_id varchar(20) NOT NULL,
+    student_name varchar(30) Not Null,
+    dob Date default (Current_Date),
+    age int default 0,
+    dept_id varchar(20) ,
+    primary key(student_id),
+    foreign key  (dept_id) references Department_Naveen(dept_id)
+);
+
+
+insert into students_Naveen(student_id,student_name,dob,age,dept_id) values("S1","Abdul","2002-04-06",24,"D1");
+
+
+insert into students_Naveen(student_id,student_name,age,dept_id) values("S2","Shreyas",24,"D2");
+
+insert into students_Naveen(student_id,student_name,dob,age,dept_id) values("S3","Ali","2002-04-06",24,"D1");
+
+insert into students_Naveen(student_id,student_name,dob,age,dept_id) values("S4","Naveen","2004-04-06",20,"D1");
+
+select * from students;
+
+alter table courses_Naveen
+add column duration int default 8;
+
+select * from courses_Naveen;
+
+
+update students
+set student_name="sneha reddy"
+where student_id="S2";
+
+delete from courses
+where course_id="C3";
+
+select *
+from students_Naveen
+where age>21;
+
+select student_name
+from students_Naveen
+order by age;
+
+select *
+from students_Naveen 
+order by age
+limit 2;
+
+select d.dept_id,d.dept_name,count(s.student_id) as no_of_students
+from Department_Naveen as d
+left join students_Naveen as s on s.dept_id=d.dept_id
+group by d.dept_id,d.dept_name;
+
+select d.dept_id,d.dept_name,count(s.student_id) as no_of_students,ifnull(Round(avg(s.age)),0) as avg_age
+from Department_Naveen as d
+left join students_Naveen as s on s.dept_id=d.dept_id
+group by d.dept_id,d.dept_name;
+
+select max(age)as maximum_age ,min(age) as minimum_age
+from students_Naveen ;
+
+
+create table accounts_Naveen(
+account_id varchar(10) primary key NOT NULL,
+holder_name varchar(50) ,
+balance int default 0
+
+);
+insert into accounts_Naveen values("34324E","ALICE",5000),("34343G","BOB","3000"),("23141E","CHARLIE",4000);
+
+
+select * from accounts_Naveen;
+
+
+
+start transaction;
+
+update accounts_Naveen 
+set balance=balance-1000
+where account_id="34324E";
+
+update accounts_Naveen
+set balance=balance+1000
+where account_id="34343G";
+
+
+rollback;
+
+select * from accounts;
+
+start TRANSACTION;
+
+update accounts_Naveen
+set balance=balance-1000
+ where account_id="34324E";
+ savepoint after_debut;
+ 
+update accounts_Naveen 
+set balance=balance+1000
+where account_id="34343G";
+
+
+update accounts_Naveen 
+set balance=balance+500
+where account_id="23141E";
+
+rollback to after_debut;
+
+commit;
