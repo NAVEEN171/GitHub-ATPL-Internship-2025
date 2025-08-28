6b0d92b89e3a81b9e30f02786a7ecd728f6f4880
NAVEEN171 <naveenkumar171837@gmail.com>
2025-08-28 17:06:28 +0530

    updated java8features

--- java8Features/src/com/java8Features/GetDayOfSpecifiedDate.java
+++ java8Features/src/com/java8Features/GetDayOfSpecifiedDate.java
@@ -6,26 +6,24 @@ import java.util.Scanner;
 
 public class GetDayOfSpecifiedDate {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
         int year;
         int month;
         int day;
-        Scanner sc=new Scanner(System.in);
-       System.out.print("Enter day : ");
-       day=sc.nextInt();
-       System.out.print("Enter month : ");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter day : ");
+        day = sc.nextInt();
+        System.out.print("Enter month : ");
 
-       month=sc.nextInt();
-       System.out.print("Enter year : ");
+        month = sc.nextInt();
+        System.out.print("Enter year : ");
 
-       year=sc.nextInt();
-       
-       Calendar cal=Calendar.getInstance();
-       LocalDate currentDate=LocalDate.of(year, month, day);
+        year = sc.nextInt();
+
+        LocalDate currentDate = LocalDate.of(year, month, day);
         System.out.println(currentDate.getDayOfWeek());
-       
-        
-	}
+
+    }
 
 }
